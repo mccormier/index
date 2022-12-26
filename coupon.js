f35f43b425009efc5d11f99f551229c5e6cce254
mccormier <cormiermac.mc85@gmail.com>
2022-12-26 16:35:47 -0500

    16.35

--- coupon.js
+++ coupon.js
@@ -1,4 +1,14 @@
+const time = new Date();
+var month=time.getMonth()+1;
+if (month == 12) {
+//
+document.getElementById("couponDiv").style.border = "3px dashed black";
 var coupon=document.getElementById('coupon');
 coupon.innerHTML='<b>Coupon</b><br />-50%';
 var couponCode=document.getElementById('couponCode');
-couponCode.innerHTML='01ZboxingDayZ10';
+couponCode.innerHTML='<i>01ZboxingDayZ10</i>';
+//
+} else {
+var cpD=document.getElementsById('couponDiv');
+	cpD.style.display="none";
+}
--- time.js
+++ time.js
@@ -1,22 +1,21 @@
+const time = new Date();
 // date part ///
-var month=x.getMonth()+1;
-var day=x.getDate();
-var year=x.getFullYear();
+var month=time.getMonth()+1;
+var day=time.getDate();
+var year=time.getFullYear();
 if (month <10 ){month='0' + month;}
 if (day <10 ){day='0' + day;}
 var x3= month+'-'+day+'-'+year;
 // time part //
-var hour=x.getHours();
-var minute=x.getMinutes();
-var second=x.getSeconds();
+var hour=time.getHours();
+var minute=time.getMinutes();
+var second=time.getSeconds();
 if(hour <10 ){hour='0'+hour;}
 if(minute <10 ) {minute='0' + minute; }
 var x3 = x3 + ' ' +  hour+':'+minute+':'+second
 //
 //
-//
 // these are the most useful ones IMO
-var time = new Date();
 time.getDate(); // returns value 1-31 for day of the month
 time.getDay(); //returns value 0-6 for day of the week
 time.getFullYear(); //returns a 4 digit value for the current year
@@ -30,7 +29,4 @@ time.toLocaleString(); //returns date and time (e.g. "9/12/2015, 6:08:25 PM")
 time.toLocaleTimeString(); //returns time (e.g. "6:08:25 PM")
 time.toLocaleDateString(); //returns date (e.g. "9/12/2015")
 //
-//
-//
-let today = new Date();
-let now = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+let now = time.getHours() + ":" + time.getMinutes() + ":" + time.getSeconds();
