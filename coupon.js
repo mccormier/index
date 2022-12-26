eb155903d52f2ca9f64dc534a529ece0024de160
mccormier <cormiermac.mc85@gmail.com>
2022-12-26 15:37:25 -0500

    15.37

---
+++ coupon.js
@@ -0,0 +1,4 @@
+var coupon=document.getElementById('coupon');
+coupon.innerHTML='<b>Coupon</b><br />-50%';
+var couponCode=document.getElementById('couponCode');
+couponCode.innerHTML='01ZboxingDayZ10';
