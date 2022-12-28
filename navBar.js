00e931625a7d939b5d92744628bbdb6ef055175a
mccormier <cormiermac.mc85@gmail.com>
2022-12-28 17:37:09 -0500

    added js nav bar script (simple js navBar txt String Var Obj)

---
+++ navBar.js
@@ -0,0 +1,61 @@
+var navBar = '';
+navBar += '<div class="navbar">';
+navBar += '<a href="/index/">Accueil</a>';
+navBar += '<a href="https://linktr.ee/mcormier">À Propos</a>';
+navBar += '<a href="/index/MCBrady">#MCBrady</a>';
+/*
+  <div class="subnav">
+    <button class="subnavbtn">Produits</button>
+    <div class="subnav-content">
+      <a href="/index/"></a>
+    </div>
+  </div>
+*/
+navBar += '<div class="subnav">';
+navBar += '<button class="subnavbtn">Produits</button>';
+navBar += '<div class="subnav-content">';
+navBar += '<a href="/index/QRcodeGenerator">QR Code Generator</a>';
+navBar += '<a href="#"></a>';
+navBar += '</div>';
+navBar += '</div>';
+navBar += '<div class="subnav">';
+navBar += '<button class="subnavbtn">Services</button>';
+navBar += '<div class="subnav-content">';
+navBar += '<a href="/index/C/inetpub/wwwroot">Internet Information Services Manager (IISS) - (WEB)</a>';
+navBar += '<a href="#recycle">Recyclage</a>';
+navBar += '<a href="#maintenance">Réparation - Maintenance - Entretien</a>';
+navBar += '<a href="#progiciel">Programmation</a>';
+navBar += '<a href="#formations">Formation Personalisée</a>';
+navBar += '<a href="https://goulet.ca/produits-numeriques/webformation/">Formation WEB Et Téléchargement de fichiers</a>';
+navBar += '<a href="https://www.teamviewer.com/fr-ca/telecharger/windows/">Express (TeamViewer)</a>';
+navBar += '</div>';
+navBar += '</div>';
+navBar += '<div class="subnav">';
+navBar += '<button class="subnavbtn">Références</button>';
+navBar += '<div class="subnav-content">';
+navBar += '<a href="#lang">Languages</a>';
+navBar += '<a href="#Snips">Snippets</a>';
+navBar += '<a href="#Templates">PortFolio</a>';
+navBar += '<a href="#modules">Modules</a>';
+navBar += '<a href="#package">Package</a>';
+navBar += '</div>';
+navBar += '</div>';
+navBar += '<div class="subnav">';
+navBar += '<button class="subnavbtn">Tools</button>';
+navBar += '<div class="subnav-content">';
+navBar += '<a href="https://tools.pdf24.org/fr/">Production de document PDF</a>';
+navBar += '</div>';
+navBar += '</div>';
+navBar += '<a href="/index/pageEnConstruction.htm">SandBox</a>';
+navBar += '<div class="subnav" style="float:right;">';
+navBar += '<button class="subnavbtn">Client(s)</button>';
+navBar += '<div class="subnav-content">';
+navBar += '<a href="#login" style="float:right;">Inscription / Connexion</a>';
+navBar += '<a href="#client-form" style="float:right;">Formulaire</a>';
+navBar += '<a href="#footer" style="float:right;">Contact</a>';
+navBar += '</div>';
+navBar += '</div>';
+navBar += '</div>';
+/*
+https://stackoverflow.com/questions/31954089/how-can-i-reuse-a-navigation-bar-on-multiple-pages
+*/
