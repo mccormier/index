7432f656ed00b46892fc6898a08f642b204398d9
mccormier <cormiermac.mc85@gmail.com>
2022-12-31 14:55:12 -0500

    14.55

---
+++ topBtn.js
@@ -0,0 +1,13 @@
+let topButton = document.getElementById("topBtn");
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    topButton.style.display = "block";
+  } else {
+    topButton.style.display = "none";
+  }
+}
+function goTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
